3780ae300bfb8e622ee06f90d0a92a7a9db340ce
Natalie Wood <lazyturtle3@gmail.com>
2021-02-01 16:03:27 -0600

    hw done

--- src/React/Pages/Welcome/Welcome.jsx
+++ src/React/Pages/Welcome/Welcome.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+/* components ---------------------------*/
+import Staff from './Staff.jsx';
+import Tabbed from './Tabbed/Tabbed.jsx';
+
 const Welcome = () => {
     return (
         <div>
             <h1>Welcome</h1>
+
+            <Tabbed />
+            <Staff />
         </div>
     )
 }
