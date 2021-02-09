dfc6905937a14eed1fb6b7b6cf516f8c5a060adc
Natalie Wood <lazyturtle3@gmail.com>
2021-02-08 18:53:10 -0600

    homework

--- src/React/Nav.jsx
+++ src/React/Nav.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faBars,
+} from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Nav = () => {
     return (
-        <NavStyled className='Nav' >
+        <NavStyled className='Nav'>
+            <FontAwesomeIcon icon={faBars} />
             <NavLink to="/" exact>Welcome</NavLink>
             <NavLink to="/Services">Services</NavLink>
             <NavLink to="/Login">Login</NavLink>
@@ -29,4 +37,5 @@ const NavStyled = styled.nav`
             background-color: #004141;
         }
     }
+
 `;
